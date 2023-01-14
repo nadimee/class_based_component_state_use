7d3220cc25886aa9b1576d5e0277f63f02094a67
nadimee <nadimeebennabdellkaderr@gmail.com>
2023-01-14 23:35:43 +0100

    Try to fix import App.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {Component} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component {
+
+    constructor(props) {
+        super(props)
+      
+        this.state = {
+            
+            fullName:"Karim ben saleh",
+            bio:"Karim 's age is 31 , he is a journalist from 2016", 
+            imgSrc:"Karim.webp",
+            profession:"journalist",
+            shows:false
+        }
+      }
+
+render(){
+
+return(
+    <div>
+       <button>Show content</button>
+       <h1>{this.state.fullName}</h1>
+       <h1>{this.state.bio}</h1>
+       <h1>{this.state.profession}</h1>
+       <img src={this.state.imgSrc} alt='Karim' />
     </div>
-  );
+)
 }
 
+
+}
+
+
 export default App;
+
